107ff23294e21e5b4fb2896569c5afbd1fcf6a47
Matheus Maia <matheussantosmaia@gmail.com>
2024-07-03 08:27:51 -0300

    add pixel lead

--- src/pages/obrigado.jsx
+++ src/pages/obrigado.jsx
@@ -7,7 +7,7 @@ import { useEffect } from "react";
 export default function Home() {
 
     useEffect(() => {
-        // fbq('track', 'Lead');
+        fbq('track', 'Lead');
     }, [])
 
     return (
